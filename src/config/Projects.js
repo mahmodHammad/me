7bf750cbd7ef04dd729924c668e7fdee69d3928b
Mahmoud Hammad <hodaman2012@gmail.com>
2020-05-14 02:50:17 +0200

    adding real data for my projects

--- src/config/Projects.js
+++ src/config/Projects.js
@@ -1,39 +1,24 @@
 export default [
   {
-    id:"moochub",
+    id: "moochub",
     img:
       "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.15752-9/96537174_2557479474474357_2643162825744187392_n.png?_nc_cat=107&_nc_sid=b96e70&_nc_ohc=iWeHK8APHhMAX-Bfz49&_nc_ht=scontent-hbe1-1.xx&oh=1f65cfe5721b242a048a8ff702ce5051&oe=5EE2786B",
     title: "MoocHub",
     body:
-      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-      links: {
-        gitHub:
-          "https://github.com/mahmodHammad/old-projects/tree/master/elite%20perl",
-        visit: "https://mahmodhammad.github.io/old-projects/elite%20perl"
-      },
-      Technologies: ["Html", "Css", "Js", "Jquery", "Bootstrap4"]
-
+      "my best work ever. contains all study materials for my faculty, in an organized, fast, and pretty looking UI,with other features like Search Wikipedia, and on MS Academics, similarity checker ,Study room, customized video player. Content is sync with google drive using drive API Its PWA, Responsive, and costed me 0$",
+    links: {
+      visit: "https://mahmodhammad.github.io/moocHub"
     },
+    Technologies: ["React", "Material-UI", "Firebase", "HTML5", "CSS3"]
+  },
+
   {
-    id:"asueng",
+    id: "elitperl",
     img:
-      "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.15752-9/96695043_531701087500481_5245105226851024896_n.png?_nc_cat=111&_nc_sid=b96e70&_nc_ohc=07TKjuEYXuoAX-OcZef&_nc_ht=scontent-hbe1-1.xx&oh=f72fc2f8f50aef39db11d295c74eff44&oe=5EE192D4",
-    title: "MoocHub",
+      "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.15752-9/96691443_564978627754404_6309108951241719808_n.png?_nc_cat=103&_nc_sid=b96e70&_nc_ohc=FNaAwEtcNroAX8c_Vbv&_nc_ht=scontent-hbe1-1.xx&oh=5b832f70445c5d63be6fa5e17ac5c445&oe=5EE2A6C8",
+    title: "ElitPerl",
     body:
-      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-      links: {
-        gitHub:
-          "https://github.com/mahmodHammad/old-projects/tree/master/elite%20perl",
-        visit: "https://mahmodhammad.github.io/old-projects/elite%20perl"
-      },
-      Technologies: ["Html", "Css", "Js", "Jquery", "Bootstrap4"]
-  },
-  {
-    id:"elitperl",
-    img:
-      "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.15752-9/96599567_668376677291420_4062225805663535104_n.png?_nc_cat=105&_nc_sid=b96e70&_nc_ohc=RBzd6qM3NuQAX-K1biB&_nc_ht=scontent-hbe1-1.xx&oh=80bf3462e8f0469eaf403f95ad6f20bc&oe=5EE2F4EC",
-    title: "ElitPerl",
-    body: "About section for a fake design,and develpment Agency. made with Html,css,bootstrap4,jQuery just for learning purposes",
+      "It's just an About section for a fake develpment Agency. it was my first projects at 2018. only for the learning purposes",
     links: {
       gitHub:
         "https://github.com/mahmodHammad/old-projects/tree/master/elite%20perl",
@@ -42,32 +27,16 @@ export default [
     Technologies: ["Html", "Css", "Js", "Jquery", "Bootstrap4"]
   },
   {
-    id:"x",
+    id: "asueng",
     img:
-      "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.15752-9/96695043_531701087500481_5245105226851024896_n.png?_nc_cat=111&_nc_sid=b96e70&_nc_ohc=07TKjuEYXuoAX-OcZef&_nc_ht=scontent-hbe1-1.xx&oh=f72fc2f8f50aef39db11d295c74eff44&oe=5EE192D4",
-    title: "ElitPerl",
-    body: "About section for a fake design,and develpment Agency. made with Html,css,bootstrap4,jQuery just for learning purposes",
-
+      "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.15752-9/96797001_590920158205626_686336117498183680_n.png?_nc_cat=100&_nc_sid=b96e70&_nc_ohc=nm_G-qtR1DYAX-pgR4A&_nc_ht=scontent-hbe1-1.xx&oh=be641005865d5e42241f179c14654e5a&oe=5EE3911E",
+    title: "AsuEngineer",
+    body:
+      " Simple lightweight Responsive website for 2nd-Electrical ASU which contains all study resources in one place, using only Vanilla Javascript",
     links: {
-      gitHub:
-        "https://github.com/mahmodHammad/old-projects/tree/master/elite%20perl",
-      visit: "https://mahmodhammad.github.io/old-projects/elite%20perl"
+      gitHub: "https://github.com/mahmodHammad/2nd-Electrical",
+      visit: "https://asu.engineer"
     },
-    Technologies: ["Html", "Css", "Js", "Jquery", "Bootstrap4"]
-  },
-  {
-    id:"y",
-    img:
-      "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.15752-9/96599567_668376677291420_4062225805663535104_n.png?_nc_cat=105&_nc_sid=b96e70&_nc_ohc=RBzd6qM3NuQAX-K1biB&_nc_ht=scontent-hbe1-1.xx&oh=80bf3462e8f0469eaf403f95ad6f20bc&oe=5EE2F4EC",
-
-    title: "ElitPerl",
-    body: "About section for a fake design,and develpment Agency. made with Html,css,bootstrap4,jQuery just for learning purposes",
-
-    links: {
-      gitHub:
-        "https://github.com/mahmodHammad/old-projects/tree/master/elite%20perl",
-      visit: "https://mahmodhammad.github.io/old-projects/elite%20perl"
-    },
-    Technologies: ["Html", "Css", "Js", "Jquery", "Bootstrap4"]
+    Technologies: ["HTML", "CSS", "JavaScript"]
   }
 ];
