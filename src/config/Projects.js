56b7f874d1e00be34ef0c8c0195ee69e94439735
Mahmoud Hammad <hodaman2012@gmail.com>
2020-05-16 14:52:57 +0200

    adding moreImages for projects

--- src/config/Projects.js
+++ src/config/Projects.js
@@ -1,9 +1,16 @@
 export default [
   {
     id: "moochub",
-    date:2020,
+    date: 2020,
     img:
       "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.15752-9/96537174_2557479474474357_2643162825744187392_n.png?_nc_cat=107&_nc_sid=b96e70&_nc_ohc=iWeHK8APHhMAX-Bfz49&_nc_ht=scontent-hbe1-1.xx&oh=1f65cfe5721b242a048a8ff702ce5051&oe=5EE2786B",
+    moreImg: [
+      "https://user-images.githubusercontent.com/41790090/82119925-99cb4680-9782-11ea-9bad-574b58ae39c7.jpg",
+      "https://user-images.githubusercontent.com/41790090/82119906-7bfde180-9782-11ea-922e-537e5678c7fd.jpg",
+      "https://user-images.githubusercontent.com/41790090/82119976-0d6d5380-9783-11ea-9eaf-0b045e39b486.png",
+      "https://user-images.githubusercontent.com/41790090/82120004-4ad1e100-9783-11ea-8ce4-7c140c116f1d.png",
+      "https://user-images.githubusercontent.com/41790090/82120148-625d9980-9784-11ea-8706-a45b6374fbb5.jpg"
+    ],
     title: "MoocHub",
     body:
       "my best work ever. contains all study materials for my faculty, in an organized, fast, and pretty looking UI,with other features like Search Wikipedia, and on MS Academics, similarity checker ,Study room, customized video player. Content is sync with google drive using drive API Its PWA, Responsive, and costed me 0$",
@@ -12,12 +19,33 @@ export default [
     },
     Technologies: ["React", "Material-UI", "Firebase", "HTML5", "CSS3"]
   },
+  {
+    id: "Pizza",
+    date: 2019,
+    img:
+      "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.15752-9/97039241_974672406263278_8273356327888093184_n.png?_nc_cat=111&_nc_sid=b96e70&_nc_ohc=j8m2fSPAYloAX8J-To6&_nc_ht=scontent-hbe1-1.xx&oh=f70b64b779cbdb0ea83429835cdd002a&oe=5EE2420F",
+    moreImg: [
+      "https://user-images.githubusercontent.com/41790090/82119413-9fbf2880-977e-11ea-97d2-05e5bb4cc512.jpg"
+    ],
 
+    title: "Pizza resturent",
+    body:
+      " Simple lightweight Responsive website for a fake Italian pizza website with a beautiful menu  , to demonstrate my skills on web development.",
+    links: {
+      gitHub: "https://github.com/mahmodHammad/pizza-Restaurent",
+      visit: "https://mahmodhammad.github.io/pizza-Restaurent/"
+    },
+    Technologies: ["HTML", "CSS", "JavaScript", "boostrap", "jQuery"]
+  },
   {
     id: "elitperl",
-    date:2018,
+    date: 2018,
     img:
       "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.15752-9/96691443_564978627754404_6309108951241719808_n.png?_nc_cat=103&_nc_sid=b96e70&_nc_ohc=FNaAwEtcNroAX8c_Vbv&_nc_ht=scontent-hbe1-1.xx&oh=5b832f70445c5d63be6fa5e17ac5c445&oe=5EE2A6C8",
+    moreImg: [
+      "https://user-images.githubusercontent.com/41790090/82119767-6936dd00-9781-11ea-97ef-af246c15f63d.jpg"
+    ],
+
     title: "ElitPerl",
     body:
       "It's just an About section for a fake develpment Agency. it was my first projects at 2018. only for the learning purposes",
@@ -26,20 +54,6 @@ export default [
         "https://github.com/mahmodHammad/old-projects/tree/master/elite%20perl",
       visit: "https://mahmodhammad.github.io/old-projects/elite%20perl"
     },
-    Technologies: ["Html", "Css", "Js", "Jquery", "Bootstrap4"]
-  },
-  {
-    id: "asueng",
-    date:2019,
-    img:
-      "https://scontent-hbe1-1.xx.fbcdn.net/v/t1.15752-9/96797001_590920158205626_686336117498183680_n.png?_nc_cat=100&_nc_sid=b96e70&_nc_ohc=nm_G-qtR1DYAX-pgR4A&_nc_ht=scontent-hbe1-1.xx&oh=be641005865d5e42241f179c14654e5a&oe=5EE3911E",
-    title: "AsuEngineer",
-    body:
-      " Simple lightweight Responsive website for 2nd-Electrical ASU which contains all study resources in one place, using only Vanilla Javascript",
-    links: {
-      gitHub: "https://github.com/mahmodHammad/2nd-Electrical",
-      visit: "https://asu.engineer"
-    },
-    Technologies: ["HTML", "CSS", "JavaScript"]
+    Technologies: ["Html", "Css", "JavaScript", "Jquery", "Bootstrap4"]
   }
 ];
