ee86352de02f454137a79993bf9933dbf95ddf94
Mahmoud Hammad <hodaman2012@gmail.com>
2020-06-10 18:42:58 +0200

    sidecontact

--- src/pages/Home/components/Contact/Contact.jsx
+++ src/pages/Home/components/Contact/Contact.jsx
@@ -4,13 +4,39 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import ContactForm from "./ContactForm";
 import ContactMethods from "./ContactMethods";
+import IconButton from "@material-ui/core/IconButton";
+import Mail from "../../../../Icons/Mail";
+import GH from "../../../../Icons/GH";
+import FB from "../../../../Icons/FB";
+import Twitter from "../../../../Icons/Twiter";
+import LI from "../../../../Icons/LI";
+
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    paddingTop: 60,
-    paddingBottom: 0,
+    paddingTop: 90,
+    paddingBottom: 10,
     marginTop: 140,
     background: theme.palette.contact.bg
+  },
+  iconLable: { marginLeft: 15 },
+  connect: {
+    background: "linear-gradient(90deg, #1f1f1f 5%, #0000 95%)",
+    borderLeft: `1px solid ${theme.palette.primary.main}`,
+    borderRadius: 20,
+    textAlign: "Left",
+    paddingTop: 16,
+    paddingBottom: 16
+  },
+  icons: {
+    color: theme.palette.primary.main,
+    fontSize: 29,
+
+    margin: "12px 0",
+    "&:hover": {
+      color: "#fff"
+    }
   },
   header: {
     fontSize: "1.8rem",
@@ -44,21 +70,66 @@ export default function Navbar() {
   return (
     <div id="contact" className={classes.root}>
       <Container maxWidth="lg">
-        <Typography variant="h1" gutterBottom className={classes.header}>
-          Get in touch
-        </Typography>
-        <Typography variant="h2" gutterBottom className={classes.body}>
-          I am interested in freelance opportunities, espacially ambitious or
-          large projects.
-          <span className={classes.hide}>
-            <br />
-          </span>{" "}
-          However, if you have other request or question. don't hesitate to
-          contact me.
-        </Typography>
-        <ContactForm />
+        <Grid container spacing={4}>
+          <Grid item xs={12} lg={10}>
+            <Typography variant="h1" gutterBottom className={classes.header}>
+              Get in touch
+            </Typography>
+            <Typography variant="h2" gutterBottom className={classes.body}>
+              i would like to create things with fun{" "}
+              <span aria-label="smile" role="img">
+                😄
+              </span>{" "}
+              <br /> Feel free to say hello!
+            </Typography>
+
+            <ContactForm />
+          </Grid>
+          <Grid conteiner item xs={12} lg={2} className={classes.connect}>
+            <Grid item xs={12}>
+              <IconButton component="a" href="mailto:ma7mod.7ammad@gmail.com">
+                <Mail className={classes.icons} />{" "}
+                <Typography className={classes.iconLable} color="primary">
+                  Email
+                </Typography>
+              </IconButton>
+            </Grid>
+            <Grid item xs={12}>
+              <IconButton component="a" href="mailto:ma7mod.7ammad@gmail.com">
+                <LI className={classes.icons} />{" "}
+                <Typography className={classes.iconLable} color="primary">
+                  LinkedIn
+                </Typography>
+              </IconButton>
+            </Grid>
+            <Grid item xs={12}>
+              <IconButton component="a" href="mailto:ma7mod.7ammad@gmail.com">
+                <GH className={classes.icons} />
+                <Typography className={classes.iconLable} color="primary">
+                  Github
+                </Typography>
+              </IconButton>
+            </Grid>
+            <Grid item xs={12}>
+              <IconButton component="a" href="mailto:ma7mod.7ammad@gmail.com">
+                <FB className={classes.icons} />
+                <Typography className={classes.iconLable} color="primary">
+                  Facebook
+                </Typography>
+              </IconButton>
+            </Grid>
+            <Grid item xs={12}>
+              <IconButton component="a" href="mailto:ma7mod.7ammad@gmail.com">
+                <Twitter className={classes.icons} />
+                <Typography className={classes.iconLable} color="primary">
+                  Twitter
+                </Typography>
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Grid>
       </Container>
-        <ContactMethods />
+      <ContactMethods />
     </div>
   );
 }
